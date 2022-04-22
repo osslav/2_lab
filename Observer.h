1bbdf72667c381cd6b2834093b175da63fbff7e0
slava <osslav@mail.com>
2022-04-22 14:13:25 +0700

    add class for observers

---
+++ Observer.h
@@ -0,0 +1,19 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+#include <QString>
+
+class IObserverFile
+{
+    virtual void update(int statusFile, int sizeFile) = 0;
+};
+
+class ObserverFile : public IObserverFile
+{
+    QString name;
+    int size;
+    ObserverFile(QString file);
+    void update(int statusFile, int sizeFile);
+};
+
+#endif // OBSERVER_H
