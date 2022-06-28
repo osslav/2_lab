ad1265c0907083381ededdf838c18150340e4d09
slava <osslav@mail.com>
2022-06-28 17:46:30 +0700

    fix test example

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@ int main()              //тестовый пример
 {             
     QString fileName = "test.txt";                                  //задаем имя файла и создаем объект MyFile и 2 наблюдателя
     MyFile testFile(fileName);
-    ObserverFile fistObserver(fileName), secondObserver(fileName);
+    ObserverFile fistObserver(fileName + " first"), secondObserver(fileName + " second");
 
-    testFile.attach(&fistObserver);                                 //привязываем наблюдателей к файлу
-    testFile.attach(&secondObserver);
+    testFile.attach(&fistObserver);                                 //привязываем первого наблюдателя к файлу
+    testFile.notify(true, 256);                                     //тестовый вызов обновления наблюдателей с переданными данными(данный взяты для примера)
 
+    testFile.attach(&secondObserver);                               //прявязываем второго наблюдателя
     testFile.updateObservers();                                     //обновляем данные в наболюдателях(двух привязанных ранее)
 
-    testFile.detach(&secondObserver);                               //отключаем одного наблюдателя
-
+    testFile.detach(&fistObserver);                                 //отключаем одного наблюдателя
     testFile.updateObservers();                                     //обновляем данные в наболюдателях(одном оставшемся)
 
     for(int i = 0; i < 120; i++)                                    //2 минуты с интервалом в 1 секунду отслеживаем состояние файла
